1e45bd306c2be7b740227ff85ee7a958eab1661d
loarsaw <amanahmed1385@gmail.com>
2023-09-10 10:58:09 +0530

    falut

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
           //   ],
           // },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
